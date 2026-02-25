0cfa13774fd841b66b9c8957082513b0e29b64b4
deepakchauhan33 <deep651965@gmail.com>
2026-02-25 23:46:03 +0530

    Created footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,65 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div >
-      FOOTER
-    </div>
+    <footer>
+
+      <div className='flex'>
+
+        {/* Logo and Short Title */}
+        <div className=''>
+
+          {/* Logo */}
+          <div>
+            logo
+          </div>
+
+          <p>Your trusted source for quality products at great prices.</p>
+
+          <div>
+            <ul className='flex'>
+              <li>insta</li>
+              <li>twiter</li>
+              <li>facebook</li>
+            </ul>
+          </div>
+        </div>
+
+
+
+        {/* Quick Links */}
+        <div>
+
+          <h2>Links</h2>
+
+          <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Cart</li>
+            <li>Orders</li>
+          </ul>
+        </div>
+
+
+        {/* Categories */}
+        <div>
+
+          <h2>Categories</h2>
+
+          <ul>
+            <li>Men's</li>
+            <li>Women's</li>
+            <li>Electronics</li>
+            <li>Jewelry</li>
+          </ul>
+        </div>
+
+
+        {/* Support & Help */}
+        <div>
+          
+        </div>
+      </div>
+    </footer>
   )
 }
 
