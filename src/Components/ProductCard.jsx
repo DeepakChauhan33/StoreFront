a77ff011075632aed089026b4a3888fb5301f29c
deepakchauhan33 <deep651965@gmail.com>
2026-02-28 12:35:58 +0530

    Design Product Card

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,28 +1,56 @@
 import React from 'react'
 
 const ProductCard = ({ product }) => {
+    console.log(product)
+
+    function handleRating(rate) {
+        let rateStar = "";
+
+        for (let i = 1; i <= 5; i++) {
+            if (i <= Math.floor(rate)) {
+                rateStar += "★";
+            } else {
+                rateStar += "☆";
+            }
+        }
+        return rateStar;
+    }
 
     return (
-        <div>
+        <div className='border p-2 flex flex-col items-center rounded-xl gap-y-5 '>
 
             {/* Product IMage */}
-            <div>
-                <img src={product.image} alt={product.title} />
+            <div className='h-60 md:h-70 w-full flex items-center justify-center py-2 bg-gray-200 '>
+                <img
+                    className='h-full  object-contain hover:scale-105 transition duration-400 ease-initial'
+                    src={product.image} alt={product.title} />
             </div>
 
-            {/*Product Category Name  */}
-            <p>{product.category}</p>
 
-            {/* Product Title */}
-            <p>{product.title}</p>
+            {/* Product Info */}
 
-            {/* Product Price */}
-            <p>{product.price}</p>
+            <div className=' w-full'>
 
-            {/* Add to cart Button */}
-            <button>
-                Add to cart
-            </button>
+                {/* Product Title */}
+                <p className='text-lg font-semibold'>
+                    {product.title}
+
+                </p>
+
+
+                {/* Product Rating */}
+                <p className='text-amber-300'>
+                    {`${handleRating(product.rating.rate)}`}
+                </p>
+
+
+                <p className='text-lg font-semibold'>
+                    {`$${product.price}`}
+
+                </p>
+
+
+            </div>
         </div>
     )
 }
