09ab522d4533b7bfdcaec6d700ad8d4c9f9bac3d
deepakchauhan33 <deep651965@gmail.com>
2026-02-28 13:23:26 +0530

    add more elements inside product card and getting some layout issue inside the card

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
 
+import { CiHeart } from "react-icons/ci";
+
 const ProductCard = ({ product }) => {
-    console.log(product)
+    // console.log(product)
 
     function handleRating(rate) {
         let rateStar = "";
 
         for (let i = 1; i <= 5; i++) {
             if (i <= Math.floor(rate)) {
-                rateStar += "★";
+                rateStar += "★ ";
             } else {
-                rateStar += "☆";
+                rateStar += "☆ ";
             }
         }
         return rateStar;
     }
 
     return (
-        <div className='border p-2 flex flex-col items-center rounded-xl gap-y-5 '>
+        <div className='border p-1.5 flex flex-col items-start justify-between rounded-xl '>
 
             {/* Product IMage */}
-            <div className='h-60 md:h-70 w-full flex items-center justify-center py-2 bg-gray-200 '>
+            <div className='relative h-60 w-full rounded-xl overflow-hidden flex items-center justify-center py-2 bg-gray-200 '>
+
+                {/* Image */}
                 <img
-                    className='h-full  object-contain hover:scale-105 transition duration-400 ease-initial'
-                    src={product.image} alt={product.title} />
+                    className='h-full  object-contain overflow-hidden hover:scale-105 transition duration-400 ease-initial'
+                    src={product.image} alt={product.title}
+                />
+
+                {/* Wishlist Icon */}
+                <button className='absolute top-2 right-3 text-3xl  rounded-full p-1 bg-white shadow-md'>
+                    <CiHeart className='' />
+                </button>
             </div>
 
 
             {/* Product Info */}
 
-            <div className=' w-full'>
+            <div className='w-full h-fit flex flex-col justify-around space-y-2 px-2 mt-4 '>
 
                 {/* Product Title */}
                 <p className='text-lg font-semibold'>
@@ -39,19 +49,26 @@ const ProductCard = ({ product }) => {
 
 
                 {/* Product Rating */}
-                <p className='text-amber-300'>
-                    {`${handleRating(product.rating.rate)}`}
-                </p>
+                <div className='w-full'>
+                    <span className=' text-amber-300 text-xl font-semibold'>{`${handleRating(product.rating.rate)}`}</span>
+                    <span className='text-lg'>{`(${product.rating.count})`}</span>
+
+                </div>
 
 
-                <p className='text-lg font-semibold'>
-                    {`$${product.price}`}
+                <div className='flex justify-between w-full '>
+                    <p className='text-2xl lg:text-3xl font-extrabold'>
+                        {`$${product.price}`}
+                    </p>
 
-                </p>
-
+                    <button className='"bg-black text-white px-4 py-2 rounded-sm bg-gray-800'>
+                        Add to cart
+                    </button>
+                </div>
 
             </div>
-        </div>
+
+        </div >
     )
 }
 
