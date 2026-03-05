c83f44f38c2a7977f12983157ec9c1eff8a34ce3
deepakchauhan33 <deep651965@gmail.com>
2026-03-05 22:47:55 +0530

    Update

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import ErrorImg from "../Images/ErrorImg.svg"
+import ErrorImg from "../../Images/ErrorImg.svg"
 
 const ErrorPage = () => {
     return (
         <section
-            className="min-h-screen text-center bg-no-repeat bg-center bg-contain"
+            className="min-h-120 pt-5 text-center bg-no-repeat bg-center bg-contain"
             style={{ backgroundImage: `url(${ErrorImg})` }}
         >
-            404
+
         </section>
     )
 }
