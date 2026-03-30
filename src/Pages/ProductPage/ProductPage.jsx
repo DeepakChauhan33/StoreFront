f086c38836242306ca71847fb015c4e1a6f6d57a
deepakchauhan33 <deep651965@gmail.com>
2026-03-30 20:53:39 +0530

    Update in mobile view in Product page and skeleton loader

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -15,6 +15,8 @@ import ProductCard from '../../Components/ProductCard';
 // Toast Message
 import toast from "react-hot-toast";
 
+// Content Loader
+import ContentLoader from 'react-content-loader'
 
 // React Icons
 import { FaShield } from "react-icons/fa6";
@@ -33,7 +35,7 @@ import WishlistBtn from "../../Components/WishlistBtn";
 import Loader from "../../Components/Loader";
 
 
-const ProductsPage = () => {
+const ProductsPage = ({ ...rest }) => {
 
 
     const { id } = useParams();
@@ -47,7 +49,12 @@ const ProductsPage = () => {
 
 
 
-    if (isLoading) return <Loader />;
+    if (isLoading) return <ContentLoader height="500" style={{ width: "100%" }} viewBox="0 0 275 240" {...rest}>
+        <rect x="15" y="15" rx="4" ry="4" width="25" height="25" />
+        <rect x="15" y="50" rx="2" ry="2" width="350" height="150" />
+        <rect x="15" y="230" rx="2" ry="2" width="170" height="20" />
+        <rect x="60" y="230" rx="2" ry="2" width="170" height="20" />
+    </ContentLoader>;
 
 
 
@@ -246,7 +253,7 @@ const ProductsPage = () => {
 
                         <div className="flex flex-col md:flex-row gap-x-2 ">
 
-                            <ButtonComp width={"w-full"} product={product}>Add to cart</ButtonComp>
+                            <ButtonComp width={"w-full py-2 md:py-2.5"} product={product}>Add to cart</ButtonComp>
 
                             {/* Button to Copy Product Link  */}
 
