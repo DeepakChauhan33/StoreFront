6a5e16a96608c52c8de8a66e7fb494799c2955b1
deepakchauhan33 <deep651965@gmail.com>
2026-03-31 15:03:17 +0530

    Add loader

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,6 +13,10 @@ import { useGetProductsQuery } from '../Product/ProductApi';
 import CategoryCard from './CategoryCard';
 import ProductCard from '../../Components/ProductCard';
 
+// Content Loader
+import ContentLoader from 'react-content-loader'
+
+
 
 // React ICons
 import { TiArrowRight } from "react-icons/ti";
@@ -36,7 +40,7 @@ import bannerImg from "../../Images/E-comm banner.jpg";
 
 
 
-const Home = () => {
+const Home = ({ ...rest }) => {
 
 
 
@@ -231,10 +235,20 @@ const Home = () => {
 
 
 
-                <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 '>
-
-                    {Products?.slice(2, 6).map((item) => <ProductCard product={item} isLoading={isLoading} />)}
-
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
+                    {isLoading
+                        ? Array.from({ length: 4 }).map((_, index) => (
+                            <ContentLoader height="500" style={{ width: "100%" }} viewBox="0 0 275 240" {...rest}>
+                                <rect x="15" y="15" rx="4" ry="4" width="25" height="25" />
+                                <rect x="15" y="50" rx="2" ry="2" width="350" height="150" />
+                                <rect x="15" y="230" rx="2" ry="2" width="170" height="20" />
+                                <rect x="60" y="230" rx="2" ry="2" width="170" height="20" />
+                            </ContentLoader>
+                        ))
+                        : Products?.slice(2, 6).map((item) => (
+                            <ProductCard key={item.id} product={item} />
+                        ))
+                    }
                 </div>
             </section>
 
@@ -242,7 +256,7 @@ const Home = () => {
 
             {/* ================= READAY TO SHOP ================= */}
 
-            <section className='py-5 lg:py-20 border-t border-stone-200 bg-linear-to-r from-zinc-300/20 via-slate-50 to-zinc-300/10  drop-shadow-sm'>
+            <section className=' py-10 lg:py-20 border-t border-stone-200 bg-linear-to-r from-zinc-300/20 via-slate-50 to-zinc-300/10  drop-shadow-sm'>
 
                 <div className=' flex flex-col justify-center items-center mb-12 rounded-md  gap-y-6 py-9 '>
 
